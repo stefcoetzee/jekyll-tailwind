0a9bf289232e06225d6ffd7a746bdeaf1e84647e
Stef Coetzee <stefcoetzee@protonmail.com>
2022-01-06 07:43:21 +0200

    Remove postcss-scss

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  parser: 'postcss-scss',
-  plugins: [
+    plugins: [
     require('postcss-import'),
     require('tailwindcss'),
-    require("autoprefixer"), // example of plugin you might use
-    ...(process.env.JEKYLL_ENV == "production" // example of only using a plugin in production
+    require("autoprefixer"),                                                    // example of plugin you might use
+    ...(process.env.JEKYLL_ENV == "production"                                  // example of only using a plugin in production
       ? [require("cssnano")({ preset: "default" })]
       : [])
   ]
